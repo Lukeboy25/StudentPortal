6c44bf5f9e7b2644f1d9371a092ea058ff313944
Luke <spaansl1@hva.nl>
2019-09-28 20:00:56 +0200

    Initial commit

---
+++ app/src/main/java/hva/nl/studentportal/AddActivity.kt
@@ -0,0 +1,41 @@
+package hva.nl.studentportal
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.android.synthetic.main.content_add.*
+
+const val EXTRA_PORTAL = "EXTRA_PORTAL"
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+        setSupportActionBar(toolbar)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        fab.setOnClickListener { onSaveClick() }
+    }
+
+    private fun onSaveClick() {
+        if (etAddTitle.text.toString().isNotBlank() && etAddUrl.text.toString().isNotBlank()) {
+            val portal = Portal(etAddTitle.text.toString(), etAddUrl.text.toString())
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_PORTAL, portal)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        } else {
+            Toast.makeText(this,"There were some missing fields!"
+                , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
